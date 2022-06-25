441d13b249c0f6edca86acff1202496086ed9c65
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2022-06-25 10:41:58 +0300

    Update PackageCollector.cs

--- Data/PackageCollector.cs
+++ Data/PackageCollector.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace fastLPI.tools.decompiler.data
 {
@@ -21,6 +21,25 @@ namespace fastLPI.tools.decompiler.data
                     return item;
 
             return null;
+        }
+
+        public virtual bool DeletePackage(string PackageName)
+        {
+            foreach (Package item in this)
+            {
+                if (item.PackageName == PackageName)
+                {
+                    this.Remove(item);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public virtual Queue<Package> GetQueueInstance()
+        {
+            return new Queue<Package>(this);
         }
     }
 }
