19fb8527dccbd3f7d76a3b241951468f89c0134a
SANE5K <84617085+aleksandrresetnikov@users.noreply.github.com>
2022-12-17 13:45:54 +0300

    Improvement

---
+++ ExtraUtils/DataBuilding/JarClassesModelBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fastLPI.tools.decompiler.extra.data
+{
+    public class JarClassesModelBuilder
+    {
+
+    }
+}
